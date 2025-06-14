bf21cb174c7eed659198a80d76df03190e06c6e0
Dhaval <dhavalmakwana5494@gmail.com>
2025-06-15 04:20:24 +0530

    [ADD] Approval Attachment Zip

---
+++ sfs_approval_attachment_zip/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+{
+    # =========================
+    # Module Information
+    # =========================
+    "name": "Approval Attachment Zip",
+    "version": "18.0.1.0.0",
+    "category": "Human Resources/Approvals",
+    "summary": "Approval Attachment Zip",
+    "description": """
+        Approval Attachment Zip.
+    """,
+    "license": "LGPL-3",
+
+    # =========================
+    # Author & Maintainer
+    # =========================
+    "author": "SereneFox Solutions",
+    "website": "https://www.serenefoxsolutions.com",
+    "maintainer": "SereneFox Solutions",
+    "support": "serenefoxsolution@gmail.com",
+
+    # =========================
+    # Dependencies
+    # =========================
+    "depends": [
+        'approvals'
+    ],
+
+    # =========================
+    # Data Files (Security, Views, Wizards, Data)
+    # =========================
+    "data": [
+        'data//server_action.xml'
+    ],
+
+    # =========================
+    # Web Assets (CSS, JS, etc.)
+    # =========================
+    "assets": {},
+
+    # =========================
+    # Technical Settings
+    # =========================
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
---
+++ sfs_approval_attachment_zip/models/approval_request.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import base64
+import zipfile
+from io import BytesIO
+import re
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class ApprovalRequest(models.Model):
+    _inherit = 'approval.request'
+
+    def sanitize_filename(self, name):
+        """This method replace any symbol to underscore for file name"""
+        return re.sub(r'[\\/*?:"<>|]', "_", name)
+
+    def action_attachments_download(self):
+        """This method generate the zip file of attachment and combine in 1 zip and download"""
+        zip_buffer = BytesIO()
+        if not self.attachment_ids:
+            return self._show_warning()
+
+        with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+            for approval_id in self.filtered(lambda a: a.attachment_ids != False):
+                attachments = self.env['ir.attachment'].search([
+                    ('res_model', '=', 'approval.request'),
+                    ('res_id', '=', approval_id.id)
+                ])
+                if attachments:
+                    sub_zip_buffer = BytesIO()
+                    with zipfile.ZipFile(sub_zip_buffer, 'w', zipfile.ZIP_DEFLATED) as sub_zip_file:
+                        for attachment in attachments:
+                            sub_zip_file.writestr(attachment.name, base64.b64decode(attachment.datas))
+                    sub_zip_buffer.seek(0)
+                    file_name = 'attachments_%s'%approval_id.id if approval_id.name == '/' else approval_id.name
+                    zip_file.writestr(self.sanitize_filename(file_name)+'.zip', sub_zip_buffer.read())
+                zip_buffer.seek(0)
+        _logger.info("Zip Create and Combine success!!!!")
+
+        if zip_buffer.getbuffer().nbytes > 0:
+            zip_data = base64.b64encode(zip_buffer.getvalue()).decode('utf-8')
+            attachment = self.env['ir.attachment'].create({
+                'name': 'Approval Attachments.zip',
+                'datas': zip_data,
+                'mimetype': 'application/zip',
+            })
+            _logger.info("Attachment Created for Zip!!!!")
+            return {
+                'type': 'ir.actions.act_url',
+                'url': f'/web/content/{attachment.id}?download=true',
+                'target': 'self',
+            }
+        else:
+            return self._show_warning()
+
+    def _show_warning(self):
+        """This method gives warning when no attachment are found"""
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': 'No Attachments',
+                'message': 'No attachments found for the Journal Entries.',
+                'type': 'warning',
+                'sticky': False,
+            }
+        }
