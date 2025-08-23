6dbbb9328b51d437bf090d2a048273703e5d7f90
Dhaval <dhavalmakwana5494@gmail.com>
2025-08-23 15:08:23 +0530

    [ADD] sfs lobah extended

---
+++ sfs_lobah_extend/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+{
+    # =========================
+    # Module Information
+    # =========================
+    "name": "SFS Payroll Extend",
+    "version": "18.0.1.0.0",
+    "category": "Human Resources/Approvals",
+    "summary": "This module customizes the payslip report name to include employee name, month and year.",
+    "description": """
+    """,
+    "license": "LGPL-3",
+
+    # =========================
+    # Author & Maintainer
+    # =========================
+    "author": "SereneFox Solutions",
+    "website": "https://www.serenefoxsolutions.com",
+    "maintainer": "SereneFox Solutions",
+    "support": "serenefoxsolution@gmail.com",
+
+    # =========================
+    # Dependencies
+    # =========================
+    "depends": [
+        'hr_payroll', 'approvals'
+    ],
+
+    # =========================
+    # Data Files (Security, Views, Wizards, Data)
+    # =========================
+    "data": [
+        'views/approval_request_template.xml',
+    ],
+
+    # =========================
+    # Web Assets (CSS, JS, etc.)
+    # =========================
+    "assets": {},
+
+    # =========================
+    # Technical Settings
+    # =========================
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
