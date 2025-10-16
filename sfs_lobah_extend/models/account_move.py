7db85ff56b008f5ca4fc4aa78ec280ae50b6cccd
SFS <serenefoxsolutions@gmail.com>
2025-10-16 10:20:12 +0530

    Added assets deletion feature flag

--- sfs_lobah_extend/models/account_move.py
+++ sfs_lobah_extend/models/account_move.py
@@ -1,12 +1,21 @@
-from odoo import api, models
+from odoo import api, models, fields
 
 
 class AccountMove(models.Model):
     """Inherit account.move model"""
     _inherit = "account.move"
 
+    disable_asset_deletion = fields.Boolean(string="Disable Assets deletion on reset to draft", default=False)
+
     def action_force_register_payment(self):
         """overide action_force_register_payment and enable payment for entry type"""
         # if any(m.move_type == 'entry' for m in self):
         #     raise UserError(_("You cannot register payments for miscellaneous entries."))
         return self.line_ids.action_register_payment()
+
+    def button_draft(self):
+        is_disable_asset_deletion = any(rec.disable_asset_deletion for rec in self)
+        if is_disable_asset_deletion:
+            ctx = dict(self.env.context, is_disable_asset_deletion=True)
+            self = self.with_context(ctx)
+        return super().button_draft()
